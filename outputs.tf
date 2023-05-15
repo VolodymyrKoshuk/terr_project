c55244fe244d10a68157990938e46c981cb89b46
volodymyr_koshuk <vovakos2001@gmail.com>
2023-05-15 21:49:21 +0300

    Fix bugs in outputs file

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,13 @@
 output "public_ip_jenkins_master" {
-    value = module.ec2_jenkins_server[*].public_ip
+    value = module.ec2_jenkins_server["first"].public_ip
 }
 
 output "public_ip_jenkins_ansible_nodes" {
-    value = module.ec2_jenkins_node_ansible[*].public_ip
+    value = module.ec2_jenkins_node_ansible["first"].public_ip
 }
 
 output "public_ip_jenkins_awscli_nodes" {
-    value = module.ec2_jenkins_node_awscli[*].public_ip
+    value = module.ec2_jenkins_node_awscli["first"].public_ip
 }
 
 output "url_ecr_nodejs_app1" {
